5d93b25330d762185bb7cd6edcfb76a2807ef4ee
generalmeow <rcbrown@gmail.com>
2017-05-09 15:49:56 -0700

    add links to social media sites/portfolio/about

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', { title: 'Express', data: data});
 });
-
-//get about page
-router.get('/about', function(req, res, next){
-  res.render('about')
-})
 
 module.exports = router;
